3d3b796a9c30efe1e1dbf94675cf601b6b195d73
David Slochower <slochower@gmail.com>
2019-02-23 15:40:56 -0800

    Adds coordinates and parameter files for each system and force field

--- write-gitignore.py
+++ write-gitignore.py
@@ -3,17 +3,23 @@ from glob import glob
 
 systems = sorted(glob("systems/*"))
 for system in systems:
-    with open(os.path.join(system, ".gitignore"), "a") as file:
+    with open(os.path.join(system, ".gitignore"), "w") as file:
         gitignore_lines = f"""
-*
+/*
+!smirnoff/
+smirnoff/*
 !smirnoff/*.mol2
 !smirnoff/smirnoff.prmtop
 !smirnoff/smirnoff.inpcrd
 
+!bgbg-tip3p/
+bgbg-tip3p/*
 !bgbg-tip3p/full.crds
 !bgbg-tip3p/full.crds
 
-!bgbg-tip3p/full.crds
-!bgbg-tip3p/full.crds
+!bg2bg2-tip3p/
+bg2bg2-tip3p/*
+!bg2bg2-tip3p/full.crds
+!bg2bg2-tip3p/full.crds
         """
         file.write(gitignore_lines)
