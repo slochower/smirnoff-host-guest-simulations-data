88fa6dfdff50fb9890d830d20d45837eea06134b
David Slochower <slochower@gmail.com>
2019-02-23 15:21:00 -0800

    Update paths, yet again

--- write-gitignore.py
+++ write-gitignore.py
@@ -5,18 +5,14 @@ systems = sorted(glob("systems/*"))
 with open(".gitignore", "a") as file:
     for system in systems:
         gitignore_lines = f"""
-systems/{system}/rsync.log
-systems/{system}/smirnoff/*
-!systems/{system}/smirnoff/*.mol2
-!systems/{system}/smirnoff/smirnoff.prmtop
-!systems/{system}/smirnoff/smirnoff.inpcrd
+!{system}/smirnoff/*.mol2
+!{system}/smirnoff/smirnoff.prmtop
+!{system}/smirnoff/smirnoff.inpcrd
 
-systems/{system}/bgbg-tip3p/*
-!systems/{system}/bgbg-tip3p/full.crds
-!systems/{system}/bgbg-tip3p/full.crds
+!{system}/bgbg-tip3p/full.crds
+!{system}/bgbg-tip3p/full.crds
 
-systems/{system}/bg2bg2-tip3p/*
-!systems/{system}/bgbg-tip3p/full.crds
-!systems/{system}/bgbg-tip3p/full.crds
+!{system}/bgbg-tip3p/full.crds
+!{system}/bgbg-tip3p/full.crds
         """
         file.write(gitignore_lines)
